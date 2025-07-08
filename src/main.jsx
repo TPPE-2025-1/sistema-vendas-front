1f618e1e0d14715b9c89b3484295ea43031bf4d6
Ana Beatriz Santos <anna.bf.santos1@gmail.com>
2025-07-08 14:51:00 -0300

    chore(entry): configura ponto de entrada em main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx"; // Importa o componente App
+import "./index.css"; // se você tiver um global
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);

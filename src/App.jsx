89a9f3a6a39babad7bf9b5e1e42ef7e595531b36
Ana Beatriz Santos <anna.bf.santos1@gmail.com>
2025-07-08 23:54:13 -0300

    feat(app): inclui componente Clientes na rota principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"; // esse caminho precisa ser exato!
+import Cliente from "./pages/Clientes";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/clientes" element={<Cliente />} />
       </Routes>
     </Router>
   );

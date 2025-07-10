9ce025faabfb61d7daa8445fd766ae7f31e00d08
Ana Beatriz Santos <anna.bf.santos1@gmail.com>
2025-07-10 14:20:58 -0300

    feat(app): configura rotas e estrutura principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home"; // esse caminho precisa ser exato!
 import Cliente from "./pages/Clientes";
+import NovoCliente from "./pages/NovoCliente";
+import EditarCliente from "./pages/EditarCliente";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/clientes" element={<Cliente />} />
+        <Route path="/clientes/novo" element={<NovoCliente />} />
+        <Route path="/clientes/editar/:cpf" element={<EditarCliente />} />
       </Routes>
     </Router>
   );

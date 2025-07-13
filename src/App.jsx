fdca07ab18447f296cd0c6c64038c53d51ae0f63
Ana Beatriz Santos <anna.bf.santos1@gmail.com>
2025-07-13 15:24:03 -0300

    feat(app): ajusta rota para histórico de compras

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import Home from "./pages/Home"; // esse caminho precisa ser exato!
 import Cliente from "./pages/Clientes";
 import NovoCliente from "./pages/NovoCliente";
 import EditarCliente from "./pages/EditarCliente";
+import Vendedores from "./pages/Vendedores";
+import NovoVendedor from "./pages/NovoVendedor";
+import EditarVendedor from "./pages/EditarVendedor";
+import Produtos from "./pages/Produtos";
+import NovoProduto from "./pages/NovoProduto";
+import EditarProduto from "./pages/EditarProduto";
+import Vendas from "./pages/Vendas";
+import NovaVenda from "./pages/NovaVenda";
+import VendaDetalhada from "./pages/VendaDetalhada";
+import HistoricoCliente from "./pages/HistoricoCliente";
 
 function App() {
   return (
@@ -12,6 +22,16 @@ function App() {
         <Route path="/clientes" element={<Cliente />} />
         <Route path="/clientes/novo" element={<NovoCliente />} />
         <Route path="/clientes/editar/:cpf" element={<EditarCliente />} />
+        <Route path="/vendedores" element={<Vendedores />} />
+        <Route path="/vendedores/novo" element={<NovoVendedor />} />
+        <Route path="/vendedores/editar/:cpf" element={<EditarVendedor />} />
+        <Route path="/produtos" element={<Produtos />} />
+        <Route path="/produtos/novo" element={<NovoProduto />} />
+        <Route path="/produtos/editar/:id" element={<EditarProduto />} />
+        <Route path="/vendas" element={<Vendas />} />
+        <Route path="/vendas/nova" element={<NovaVenda />} />
+        <Route path="/vendas/relatorio/:id" element={<VendaDetalhada />} />
+        <Route path="/clientes/:cpf/historico" element={<HistoricoCliente />} />
       </Routes>
     </Router>
   );
